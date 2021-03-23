af5d8fd4119db887036e54b4affdb59b4df2037b
Sebahat K <karaarslansebahat@gmail.com>
2021-03-23 16:27:45 -0700

    yaml files are added

--- python/hands-on/flask-01-02-hello-world-app-&-Jinja-Template/flask-02-Jinja_Template/jinja.py
+++ python/hands-on/flask-01-02-hello-world-app-&-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -6,10 +6,14 @@ app = Flask(__name__)
 def head():
     return render_template('index.html', number = 20, number2 = 40)
 
-@app.route('/serdar')
+@app.route('/sebahat')
 def number():
     variable1 , variable2 = 23 , 45
     return render_template('body.html', num1=variable1, num2= variable2, multiplication=variable1+variable2)
+    
+@app.route ("/ikinci")
+def second():
+    return render_template("yeni.html")
 
 if __name__=='__main__':
     app.run(debug = True)
